b2672e33aae7391daf1502d2236ad1998e999859
Sreang Rathanak <sreangrathanak@yahoo.com>
2023-06-23 16:35:13 +0700

    Case 1 implement

--- case.py
+++ case.py
@@ -1,8 +1,13 @@
 # - It returns "Fizz" if parameter(num) is divisible by 3.
+# - It returns "Buzz" if parameter(num) is divisible by 5.
 def fizz(num):
     if num % 3 == 0:
         return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+
     return str(num)
 
 print(fizz(3))
 print(fizz(5))
+print(fizz(7))
